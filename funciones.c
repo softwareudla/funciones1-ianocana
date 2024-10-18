fc2c4d3c937179f65a8dfbb9e014b39e17ee02cb
ian.ocana <ian.ocana@udla.edu.ec>
2024-10-17 20:17:53 -0500

    Taller

--- funciones.c
+++ funciones.c
@@ -1,62 +1,27 @@
 #include <stdio.h>
-int LeerNumeroProductos (int num);
-void AsignacionNombrePrecio (int num, int valor);
+#include <string.h>
+#include "funciones.h"
+
 
 int main (int argc, char *argv[]) {
     int valor;
-    float total;
+    float total, promedio, valorbuscado;
     int num;
    
     valor = LeerNumeroProductos(num);
-    char nombresProd[valor][30];
-    float precioProd[valor];
+    AsignacionNombrePrecio (valor);
     total = Total (valor);
+  
+    
+    printf("El valor total de los productos es: %.2f \n", total);
+    Maximo (valor);
+    Minimo (valor);
+   
+    promedio = Promedio (total, valor);
 
+    printf ("El valor promedio de los productos es de: %.2f \n", promedio);
+    buscar (valor); 
+    
     return 0;
 }
 
-int LeerNumeroProductos (int num) {
-    do {
-        printf("Ingrese la cantidad de productos que desea registrar: \n");
-        scanf("%d", &num);
-    } while(num>0 && num<=10);
-    return num;
-}
-void AsignacionNombrePrecio ( int num, int valor) {
-    for (int i = 0; i < num; i++)
-    {   
-        char nombresProd[valor][30];
-        float precioProd[valor];
-        printf("Ingrese el nombre del producto %d", i+1);
-        scanf("%s", nombresProd[i]);
-        printf ("Ingrese el valor del producto %d", i+1);
-        scanf ("%f", precioProd[i]);
-        
-    }
-    
-}
-float Total (int valor) {
-    float total;
-    float precioProd[valor];
-    for( int i = 0; i < valor; i++)
-    {
-        total= precioProd[i] + total;
-    }
-    return total;
-}  
-float Maximo (int valor) {
-    float precioProd[valor];
-    float nummax;
-    if (i == 0) 
-    {
-       nummax = precioProd[i];
-    } 
-    else 
-    {
-       if (precioProd[i] > nummax)
-       {
-           nummax = precioProd[i];
-       }
-    }
-    return nummax;
-}
