1fa865fdd0f8b749b52bd33be02c8e46e17039b1
ian.ocana <ian.ocana@udla.edu.ec>
2024-10-20 21:42:46 -0500

    Actual

--- funciones.c
+++ funciones.c
@@ -3,25 +3,114 @@
 #include "funciones.h"
 
 
-int main (int argc, char *argv[]) {
-    int valor;
-    float total, promedio, valorbuscado;
-    int num;
-   
-    valor = LeerNumeroProductos(num);
-    AsignacionNombrePrecio (valor);
-    total = Total (valor);
-  
-    
-    printf("El valor total de los productos es: %.2f \n", total);
-    Maximo (valor);
-    Minimo (valor);
-   
-    promedio = Promedio (total, valor);
+/*char nombresProd[10][30];
+float precioProd[10];*/
 
-    printf ("El valor promedio de los productos es de: %.2f \n", promedio);
-    buscar (valor); 
-    
-    return 0;
+int leerNumeroProductos (int num) {
+    do {
+        printf("Ingrese la cantidad de productos que desea registrar: \n");
+        scanf("%d", &num);
+    } while(num<=0 || num>10);
+    return num;
 }
 
+void asignacionNombrePrecio (int num) {
+    for (int i = 0; i < num; i++)
+    {   
+        printf("---------------------------------------\n");
+        printf("Ingrese el nombre del producto %d: \n", i+1);
+        scanf("%s", &nombresProd[i]);
+        printf ("Ingrese el precio del producto %d:\n", i+1);
+        scanf ("%f", &precioProd[i]);
+        
+    }
+    
+}
+
+float Total (int valor) {
+    float total;
+    for( int i = 0; i < valor; i++)
+    {
+        total= precioProd[i] + total;
+    }
+    return total;
+}  
+
+void maximo (int valor) {
+    float nummax;
+    char nombre[30];
+    for (int i = 0; i < valor; i++)
+    {
+       if (i == 0) 
+    {
+       nummax = precioProd[i];
+       strcpy(nombre, nombresProd[i]) ;
+    } 
+    else 
+    {
+       if (precioProd[i] > nummax)
+       {
+           nummax = precioProd[i];
+           strcpy(nombre, nombresProd[i]) ;
+       }
+    }
+    }
+    printf("El producto mas caro es %s con precio de: %.2f \n", nombre, nummax);
+}
+
+void minimo (int valor) {
+    float nummin;
+    char nombre[30];
+
+    for (int i = 0; i < valor; i++)
+    {
+       if ( i == 0) 
+    {
+       nummin = precioProd[i];
+       strcpy(nombre, nombresProd[i]) ;
+    } 
+    else 
+    {
+       if (precioProd[i] < nummin)
+       {
+           nummin = precioProd[i];
+           strcpy(nombre, nombresProd[i]) ;
+       }
+    }
+    }
+    
+    printf("El producto mas barato es %s con precio de: %.2f \n", nombre, nummin);
+}
+
+float promedio (float total, int valor) {
+    float promedio;
+    promedio = total/valor;
+    return promedio;
+}
+
+int buscar (int valor){
+    float precio; 
+    char nombre[30];
+    int indice;
+    int bandera;
+    printf("Ingrese el nombre del producto a buscar: \n");
+    scanf("%s", &nombre);
+    bandera =0;
+    for (int i = 0; i < valor; i++)
+    {
+        if (strcmp(nombresProd[i], nombre)==0)
+        {
+          printf ("El precio del producto buscado %s es de: %.2f \n",nombre, precioProd[i] );
+          return 0;
+        }
+        else{
+            bandera = 1;
+        }
+
+        
+    }
+     if (bandera = 1){
+        printf ("El producto %s no esta en el sistema \n",nombre );
+     }
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "funciones.h"
+
+
+int main (int argc, char *argv[]) {
+    int valor;
+    float total, prom, valorbuscado;
+    int num;
+   
+    valor = leerNumeroProductos(num);
+    asignacionNombrePrecio (valor);
+    total = Total (valor);
+  
+    
+    printf("El valor total de los productos es: %.2f \n", total);
+    maximo (valor);
+    minimo (valor);
+   
+    prom = promedio (total, valor);
+
+    printf ("El valor promedio de los productos es de: %.2f \n", prom);
+    buscar (valor); 
+    
+    return 0;
+}
+
