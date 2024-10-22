7bfe66347708a25de449131d09cacdf31e4a0401
ian.ocana <ian.ocana@udla.edu.ec>
2024-10-22 09:11:34 -0500

    tallerclase

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 int main (int argc, char *argv[]) {
     int valor;
+    char conf;
     float total, prom, valorbuscado;
     int num;
    
@@ -12,6 +13,7 @@ int main (int argc, char *argv[]) {
     total = Total (valor);
   
     
+    maximo (valor);
     printf("El valor total de los productos es: %.2f \n", total);
     maximo (valor);
     minimo (valor);
@@ -19,7 +21,11 @@ int main (int argc, char *argv[]) {
     prom = promedio (total, valor);
 
     printf ("El valor promedio de los productos es de: %.2f \n", prom);
+    printf("---------------------------------------\n");
     buscar (valor); 
+
+    printf ("Gracias por utilizar este programa");
+    scanf("%s", conf);
     
     return 0;
 }
